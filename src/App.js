da5a812b606bde89344df81e54d95620485e05a5
DESKTOP-KVKG9CT\Asim <asimmehmood807@gmail.com>
2021-01-25 18:49:58 -0800

    maintained project structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp/SignUp";
+import Dashboard from "./components/Dashboard/Dashboard";
+
+import { BrowserRouter, Route, Switch, Router, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>     
+      <BrowserRouter>
+        <Switch>
+          <Route path="/Login" exact component={Login} />
+          <Route path="/SignUp" exact component={SignUp} />
+          <Route path="/" exact component={Dashboard} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SideBar from '../SideBar/SideBar'
+import Header from '../Header/Header'
+
+export default function Dashboard() {
+  return (
+    <div>
+        <Header/>
+    </div>
+  );
+}
