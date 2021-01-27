16db98f08eb995da7e693edcb4c4197008b05222
Sarmad <sarmadali0004@gmail.com>
2021-01-27 07:47:45 -0800

    Set FIles

---
+++ src/Services/data.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+import { BASE_URL1, LOGIN_URL, BASE_URL1,FETCH_BRAND  } from "../Utilities/constant";
+import { NETWORK_ERROR } from "../Utilities/constant";
+
+export async function loginRequest(obj) {
+  try {
+    const url = BASE_URL1 + LOGIN_URL;
+    return await axios.post(url, obj);
+  } catch (e) {
+    return NETWORK_ERROR;
+  }
+}
+
+export async function FetchBrandRequest(token) {
+  try {
+    const url = BASE_URL1 + FETCH_BRAND;
+    const config = {
+      headers: {
+        "x-access-token": "Bearer " + token,
+      },
+    };
+    return await axios.post(url, null, config);
+  } catch (error) {
+    return error;
+  }
+}
